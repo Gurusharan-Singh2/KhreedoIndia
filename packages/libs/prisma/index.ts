308dcbc7da87f7dcb6a940edc2f704f3a0d6e172
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-05-14 20:38:58 +0530

    Fix Prisma client initialization for WebAssembly environment

--- packages/libs/prisma/index.ts
+++ packages/libs/prisma/index.ts
@@ -1,20 +1,5 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
-declare global {
-  namespace globalThis {
-    var prismadb: PrismaClient
-  }
-}
+let prisma: PrismaClient = new PrismaClient();
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV !== 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prismadb) {
-    global.prismadb = new PrismaClient();
-  }
-  prisma = global.prismadb;
-}
-
-export default prisma
+export default prisma;
