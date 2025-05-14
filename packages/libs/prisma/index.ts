9a80b42b3aa6ecdad59dd37b0ba6d44cc4b6c992
Gurusharan Singh <gurusharansingh2222@gmail.com>
2025-05-14 20:41:44 +0530

    Fix build configuration and Prisma client initialization

--- packages/libs/prisma/index.ts
+++ packages/libs/prisma/index.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient = new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export default prisma;
